1f7dbbf04952df9d1d9c11fd95c64c3ed415946d
yikuanli <yikuan.li@wrh.ox.ac.uk>
2020-12-25 16:53:03 +0000

    add model initialization

--- models/mlmbyol.py
+++ models/mlmbyol.py
@@ -1,4 +1,4 @@
-from models.parts.blocks import BertPooler, BertEncoder
+from models.parts.blocks import BertPooler, BertEncoder, BertLayerNorm
 from models.parts.embeddings import Embedding
 import torch.nn as nn
 import pytorch_lightning as pl
@@ -133,6 +133,21 @@ class HiBEHRT(nn.Module):
 
         self.projector = MLP(params)
         self.predictor = MLP(params)
+
+        self.apply(self.init_bert_weights)
+
+    def init_bert_weights(self, module):
+        """ Initialize the weights.
+        """
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            # cf https://github.com/pytorch/pytorch/pull/5617
+            module.weight.data.normal_(mean=0.0, std=self.params['initializer_range'])
+        elif isinstance(module, BertLayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
 
     def forward(self, record, age, seg, position, att_mask, h_att_mask, bournilli_mask, if_mask):
         # bournilli = Bernoulli(torch.ones_like(h_att_mask) * prob)
