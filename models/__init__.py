751f025315e6029f5c57549df28e83db498cadaf
yikuanli <yikuan.li@wrh.ox.ac.uk>
2021-01-09 20:37:33 +0000

    import EHR2VecFintuneTest

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from models.hibehrt import EHR2Vec
 from models.finetune import EHR2VecFineTune
 from models.mlmbyol import SSLMLMBYOL
 from models.adapter import EHR2VecAdaptorFineTune
+from models.finetuneTest import EHR2VecFinetuneTest
