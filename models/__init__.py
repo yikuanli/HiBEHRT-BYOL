f2031cc2c9b68446862d1684a2d0cda4726a2e6d
yikuanli <yikuan.li@wrh.ox.ac.uk>
2021-01-11 13:55:29 +0000

    add adaptor test module

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from models.finetune import EHR2VecFineTune
 from models.mlmbyol import SSLMLMBYOL
 from models.adapter import EHR2VecAdaptorFineTune
 from models.finetuneTest import EHR2VecFinetuneTest
+from models.adapterTest import EHR2VecAdapterTest
