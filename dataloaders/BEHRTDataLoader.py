50540de3649b8b56a25310c4763e6e027dfa2971
yikuanli <yikuan.li@wrh.ox.ac.uk>
2021-04-23 16:08:31 +0100

    change subgroup analysis

--- dataloaders/BEHRTDataLoader.py
+++ dataloaders/BEHRTDataLoader.py
@@ -86,14 +86,16 @@ def BEHRTDataLoader(params):
                 data = data[data[key] > 1]
             data = data.reset_index(drop=True)
 
-        if params['min_len'] is not None:
+        if params['len_range'] is not None:
             data['len'] = data.code.apply(lambda x: len(x))
-            data = data[data['len']>params['min_len']]
+            data = data[data['len'] > params['len_range']['min_len']]
+            data = data[data['len'] < params['len_range']['max_len']]
             data = data.reset_index(drop=True)
 
-        if params['min_year'] is not None:
-            data['duration'] = data.age.apply(lambda x: int(x[-1])-int(x[0]))
-            data = data[data['duration'] > params['min_year']]
+        if params['year_range'] is not None:
+            data['duration'] = data.age.apply(lambda x: int(x[-1]) - int(x[0]))
+            data = data[data['duration'] > params['year_range']['min_year']]
+            data = data[data['duration'] < params['year_range']['max_year']]
             data = data.reset_index(drop=True)
 
         print('data size:', len(data))
