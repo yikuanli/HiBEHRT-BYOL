98e3434be60a78e7e652fb9e01ea83eb99950df7
Yikuan Li <Yikuan.li@wrh.ox.ac.uk>
2022-09-13 14:50:43 +0100

    add more layers of hibehrt

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,4 @@ from models.finetune import EHR2VecFinetune
 from models.behrt import BEHRT2Vec
 from models.medbert import MEDBEHRT2Vec
 from models.cehrbert import CEHRBEHRT2Vec
+from models.multiHibehrt import MultiEHR2Vec
